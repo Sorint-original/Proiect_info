8fbf39946d11404afe6bf16d2e0d60a990cbac7d
Sorint <pssorin13@gmail.com>
2022-03-21 18:35:22 +0200

    progres

---
+++ JocS/JocS/Player.py
@@ -0,0 +1,107 @@
+import pygame
+
+
+class control :
+    def __init__ (self,source) :
+        if source != "Keyboard" and source != "Unknown" :
+            #Valorile pe care le vom folosi pentru butoane
+            self.NR_LB = 4
+            self.NR_RB = 5
+            #Valorile pe care le vom folosi pentru axe [ AXA X , AXA Y ]
+            self.NR_AX_LJ = [0,1]
+            self.NR_AX_RJ = [2,3]
+            self.NR_AXLT = 4
+            self.NR_AXRT = 5
+            #Butoanele apasate de controller
+            self.LB = False
+            self.RB = False
+            #Trigarele sunt axe dar pentru joc le vom trata ca butoane
+            self.LT = False
+            self.RT = False
+            #Valorile de la axele joystickurilor [ AXA X , AXA Y ]
+            self.LJ = [0,0]
+            self.RJ = [0,0]
+        elif source != "Unknown" :
+            #Tastele folosite de player la tastatura
+            self.taste = {pygame.K_w:False, pygame.K_a:False, pygame.K_s:False, pygame.K_d:False, pygame.K_q:False, pygame.K_e:False}
+            #Coordonatele mousului
+            self.Mouse = [0,0]
+            #BUTOANELE DE PE MOUSE 0 LeftClick 1 Middlemouse button 2 Right click
+            self.MouseButtons = [False , False , False]
+
+
+class player:
+    #Initializarea obiectului
+    def __init__(self,BIMG,UIMG):
+        #The images for the character
+        self.Bottom_image = BIMG
+        self.Upper_image = UIMG
+        # The Variables used for the lobby
+        self.Selected = False
+        self.Source = "Unknown"
+        self.Exit_cooldown = 0
+        self.Ready = False
+        self.configuring = False
+        #Controalele pentru pleyer (toate butoanele apasate,configurarea pentru controlar)
+        self.Control = control(self.Source)
+    #Functie de resetat controalele pleyerului
+    def reset_control (self) :
+        self.Control = control (self.Source)
+    #Se verifica ce schimbari cauzeaza pentru player eventul
+    def update_input (self , event) :
+        if self.Source !="Keyboard":
+            #Pentru Controller
+            if event.type == pygame.JOYBUTTONDOWN :
+                if event.button == self.Control.NR_LB :
+                    self.Control.LB = True
+                elif event.button == slef.Control.NR_RB :
+                    self.Control.RB = True
+            elif event.type == pygame.JOYBUTTONUP :
+                if event.button == self.Control.NR_LB :
+                    self.Control.LB = False
+                elif event.button == slef.Control.NR_RB :
+                    self.Control.RB = False
+            elif event.type == pygame.JOYAXISMOTION :
+                if event.axis == self.Control.NR_AXLT :
+                    if event.value == -1 :
+                        self.Control.LT = False
+                    else :
+                        self.Control.LT = True
+                elif event.axis == self.Control.NR_AXRT :
+                    if event.value == -1 :
+                        self.Control.RT = False
+                    else :
+                        self.Control.RT = True
+                elif event.axis == self.Control.NR_AX_LJ[0] :
+                    self.Control.LJ[0] = event.value
+                elif event.axis == self.Control.NR_AX_LJ[1] :
+                    self.Control.LJ[1] = event.value 
+                elif event.axis == self.Control.NR_AX_RJ[0] :
+                    self.Control.RJ[0] = event.value 
+                elif event.axis == self.Control.NR_AX_RJ[1] :
+                    self.Control.RJ[1] = event.value 
+        else :
+            #Petreu Tastatura si mouse
+            try :
+                if event.type == pygame.KEYDOWN :
+                    self.Control.taste[event.key] = True
+                elif event.type == pygame.KEYUP :
+                    self.Control.tatse[event.key] = False 
+            except :
+                if event.type == pygame.MOUSEMOTION :
+                    self.Control.Mouse = event.pos
+                elif event.type == pygame.MOUSEBUTTONDOWN :
+                    self.Control.MouseButtons[event.button] = True
+                elif event.type == pygame.MOUSEBUTTONUP :
+                    self.Control.MouseButtons[event.button] = False
+    #Functie folosita doar in lobby
+    def exit_update (self) :
+        if self.Exit_cooldown == 0 and self.Control.LB == True and self.configuring == False :
+            self.Exit_cooldown = 120 
+        if self.Exit_cooldown > 0 and self.Control.LB == True :
+            self.Exit_cooldown -= 1
+            if self.Exit_cooldown == 0 and self.configuring == False:
+                return 1
+        elif self.Exit_cooldown > 0 :
+            self.Exit_cooldown = 0
+        return 0
--- JocS/JocS/Pleyer.py
+++
@@ -1,7 +0,0 @@
-class pleyer(object):
-    #Clasa de Pleyeri
-    def __init__(self,BIMG,UIMG):
-        self.Control = "Unknown"
-        self.Redy = False
-        self.Bottom_image = BIMG
-        self.Upper_image = UIMG
